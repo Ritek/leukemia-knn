652368da2582c149154e52a5b71334116ebbe730
Wojciech Rithaler <writhaler@gmail.com>
2020-05-23 22:58:42 +0200

    Added confusion matrix

--- src/analysis/kmeans.py
+++ src/analysis/kmeans.py
@@ -31,21 +31,25 @@ leukemia_data.drop('class', axis=1, inplace=True)
 leukemia_data.rename(columns=symptoms_name_dict, inplace=True)
 leukimia_full.rename(columns=symptoms_name_dict, inplace=True)
 
+"""
 # === kmeans clustering ===
 clusters = 10
 kmeans = KMeans(n_clusters=clusters)
 kmeans.fit(leukemia_data)
 #print(kmeans.labels_)
 # === / kmeans clustering ===
+"""
 
 # === kmeans testing ===
 # Kolmogrov-Smiernov -> 11, 17, 12, 18, 4, 16, 1, 8, 13, 14, 20, 5, 7
 # SelectKBest ->        4, 17, 19, 2, 14, 18, 12, 15, 6, 10, 13, 20
-feature_index = [4, 17, 19, 2, 14, 18, 12, 15, 6, 10, 13, 20] # indexes of features to use
+feature_index = [10] # indexes of features to use
 leuk_class = leukimia_full.iloc[:, 20:21].values
-leuk_rest = leukimia_full.iloc[:, feature_index].values
 
-print(leuk_rest)
+if len(feature_index) == 0:
+    leuk_rest = leukimia_full.iloc[:, 0:20].values
+else:
+    leuk_rest = leukimia_full.iloc[:, feature_index].values
 
 neigh = KNeighborsClassifier(n_neighbors=9)
 scores = []
@@ -53,6 +57,7 @@ scores = []
 # k = 7 -> 0.303
 # k = 5 -> 0.288
 # k = 2 -> 0.252
+con_mat_arr = []
 rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=10, random_state=36851234)
 for train_index, test_index in rskf.split(leuk_rest, leuk_class):
     #print("TRAIN:", train_index, "TEST:", test_index, sep="\n")
@@ -64,12 +69,25 @@ for train_index, test_index in rskf.split(leuk_rest, leuk_class):
     predict = neigh.predict(X_test)
     scores.append(accuracy_score(y_test, predict))
 
+    con_mat_arr.append( confusion_matrix(predict, y_train_ravel) )
+
 mean_score = np.mean(scores)
 std_score = np.std(scores)
 print("Accuracy score: %.3f (%.3f)" % (mean_score, std_score))
+print()
+print("Confusion Matrix:")
+print(con_mat_arr[0] + con_mat_arr[1] + 
+      con_mat_arr[2] + con_mat_arr[3] + 
+      con_mat_arr[4] + con_mat_arr[5] + 
+      con_mat_arr[6] + con_mat_arr[7] +
+      con_mat_arr[8] + con_mat_arr[9])
 # === / kmeans testing ===
+print()
+print(con_mat_arr[0])
 
-""" 
+
+
+"""
 # === Kolmogrov-Smiernov ===
 feature_score = []
 features_kolmogrov = leukimia_full.iloc[:, 0:20]
@@ -82,6 +100,37 @@ print(pd.DataFrame(feature_score))
 print()
 # === Kolmogrov-Smiernov ===
 
+# === Kolmogrov-Smiernov 2 ===
+feature_score2 = []
+features_kolmogrov2 = leukimia_full.iloc[:, 0:20]
+for i in range(20):
+    score = 0
+    for j in range(20):
+        if j != i:
+            score += stats.ks_2samp(features_kolmogrov2.iloc[:, i], features_kolmogrov2.iloc[:,  j]).pvalue
+
+    feature_score2.append( format(score/20, '.3') )
+
+print("Feature score Kolmogrov-Smiernov 2: ")
+print(pd.DataFrame(feature_score2))
+print()
+# === / Kolmogrov-Smiernov 2 ===
+
+# === Kolmogrov-Smiernov 3 ===
+feature_score3 = []
+features_kolmogrov3 = leukimia_full.iloc[:, 0:20]
+kol_feat_1 = pd.DataFrame(features_kolmogrov3.iloc[:, 0:10].values)
+kol_feat_2 = pd.DataFrame(features_kolmogrov3.iloc[:, 10:20].values)
+for i in range(10):
+    feature_score3.append(stats.ks_2samp(kol_feat_1.iloc[:, i], kol_feat_2.iloc[:, i]))
+
+print("Feature score Kolmogrov-Smiernov 3: ")
+print(pd.DataFrame(feature_score3))
+print()
+# === / Kolmogrov-Smiernov 3 ===
+"""
+
+"""
 # === SelectKBest ===
 X_select = leukimia_full.iloc[:, 0:20]
 #print(X_select)
